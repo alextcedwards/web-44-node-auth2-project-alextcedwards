165c2fbef4b686bd63be2659a65f1fc2d5f740a9
Alex Edwards <alextcedwards@gmail.com>
2021-08-17 19:29:15 -0600

    had a line switched, mvp done

--- api/auth/auth-middleware.js
+++ api/auth/auth-middleware.js
@@ -61,7 +61,7 @@ const checkUsernameExists = async (req, res, next) => {
   try {
     const [user] = await findBy({ username: req.body.username });
     if (!user) {
-      next({ status: 422, message: "Invalid credentials" });
+      next({ status: 401, message: "Invalid credentials" });
     } else {
       req.user = user;
       next();
@@ -90,10 +90,8 @@ const validateRoleName = (req, res, next) => {
       "message": "Role name can not be longer than 32 chars"
     }
   */
-  if (!req.body.role_name || req.body.role_name.trim()) {
-    req.role_name = "student";
-    next();
-  } else if (req.body.role_name.trim() === "admin") {
+  if(req.body.role_name){
+  if (req.body.role_name.trim() === "admin") {
     next({ status: 422, message: "Role name can not be admin" });
   } else if (req.body.role_name.trim().length > 32) {
     next({ status: 422, message: "Role name can not be longer than 32 chars" });
@@ -101,6 +99,10 @@ const validateRoleName = (req, res, next) => {
     req.role_name = req.body.role_name.trim();
     next();
   }
+} else {
+  req.role_name = "student";
+  next();
+}
 };
 
 module.exports = {
